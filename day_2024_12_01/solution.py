3e9432e917cef205ee91966cac41f4e42948f6bf
github-adelin-marrot <adelin.marrot@teads.com>
2024-12-04 16:36:18 +0100

    day_2024_12_01

---
+++ day_2024_12_01/solution.py
@@ -0,0 +1,41 @@
+import os
+
+# Historian Hysteria
+
+def common():
+    current_directory = os.path.dirname(__file__)
+    input_file_path = os.path.join(current_directory, "input.txt")
+    with open(input_file_path) as file:
+        file_data = file.read()
+
+    lines = file_data.strip().split('\n')
+    left = list()
+    right = list()
+
+    for line in lines:
+        locations = line.split()
+        left.append(int(locations[0]))
+        right.append(int(locations[-1]))
+
+    left.sort()
+    right.sort()
+
+    return left, right
+
+def part_1():
+    distance = 0
+    left, right = common()
+
+    for i in range(0, len(left)):
+        distance += abs(left[i] - right[i])
+
+    return distance
+
+def part_2():
+    score = 0
+    left, right = common()
+
+    for i in range(0, len(left)):
+        score += left[i] * right.count(left[i])
+        
+    return score
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import day_2024_12_01.solution as day_2024_12_01
 import day_2024_12_04.solution as day_2024_12_04
 
 if __name__ == "__main__":
+    print('Historian Hysteria (2024-12-01)')
+    print("Part 1:", day_2024_12_01.part_1())
+    print("Part 2:", day_2024_12_01.part_2())
     print('\nCeres Search (2024-12-04)')
     print("Part 1:", day_2024_12_04.part_1())
     print("Part 2:", day_2024_12_04.part_2())
